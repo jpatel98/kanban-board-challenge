5ebca63d2421c6e80fe77d8175a2f6d7d1df067c
Jigar Patel <jpxtel98@outlook.com>
2023-02-24 12:34:09 -0500

    Setup kanban component directory and function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.scss";
+import Kanban from "./components/Kanban/Kanban";
+
 
 function App() {
   return (
     <div style={{ padding: "50px" }}>
       <h1 style={{ marginBottom: "20px" }}>Your tasks</h1>
+      <Kanban/>
     </div>
   );
 }
---
+++ src/components/Kanban/Kanban.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Kanban = () => {
+  return (
+    <div>Kanban Component</div>
+  )
+}
+
+export default Kanban
