f4e2fc28b377e410aae1edccd607bcfc574f0f76
Jigar Patel <jpxtel98@outlook.com>
2023-02-24 12:16:03 -0500

    Export mock-data using default export

--- src/mockData.js
+++ src/mockData.js
@@ -1,4 +1,4 @@
-import { v4 as uuidv4 } from 'uuid'
+import { v4 as uuidv4 } from 'uuid';
 
 const mockData = [
     {
@@ -40,3 +40,5 @@ const mockData = [
         ]
     }
 ]
+
+export default mockData;
