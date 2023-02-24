ed20a2c7bae35b5980680a512e2ddef8348a7756
Jigar Patel <jpxtel98@outlook.com>
2023-02-24 12:13:32 -0500

    Set up mock data

---
+++ src/mockData.js
@@ -0,0 +1,42 @@
+import { v4 as uuidv4 } from 'uuid'
+
+const mockData = [
+    {
+        id: uuidv4(),
+        title: ' ✏️To do',
+        tasks: [
+            {
+                id: uuidv4(),
+                title: 'Learn TypeScript'
+            },
+            {
+                id: uuidv4(),
+                title: 'Finish assignments'
+            },
+            {
+                id: uuidv4(),
+                title: 'Workout for 45mins'
+            },
+        ]
+    },
+    {
+        id: uuidv4(),
+        title: '👩‍💻👨‍💻Work In Progress',
+        tasks: [
+            {
+                id: uuidv4(),
+                title: 'Learn GoLang'
+            }
+        ]
+    },
+    {
+        id: uuidv4(),
+        title: ' ✔️ Completed',
+        tasks: [
+            {
+                id: uuidv4(),
+                title: 'Make a kanban board with react'
+            }
+        ]
+    }
+]
