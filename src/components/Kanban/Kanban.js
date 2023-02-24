55ad3ab79a4aa8f7bd853b0e4e4b08d079914b37
Jigar Patel <jpxtel98@outlook.com>
2023-02-24 13:37:00 -0500

    Add drag feature; working on drop

--- src/components/Kanban/Kanban.js
+++ src/components/Kanban/Kanban.js
@@ -1,50 +1,73 @@
-import Card from "../Card/Card";
-import "./Kanban.scss";
-import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
-import mockData from "../../mockData";
-import { useState } from "react";
+import './Kanban.scss'
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd'
+import mockData from '../../mockData'
+import { useState } from 'react'
+import Card from '../Card/Card'
 
 const Kanban = () => {
     const [data, setData] = useState(mockData)
-    const onDragEnd = result => {
-        console.log(result);
-    }
 
-  return (
-    <DragDropContext onDragEnd={onDragEnd}>
-        <div className="kanban">
-            {
-                data.map(section => (
-                    <Droppable 
-                        key={section.id}
-                        droppableId={section.id}
-                    >
-                        {(provided) => (
-                            <div
-                                {...provided.droppableProps}
-                                className="kanban__section"
-                                ref={provided.innerRef}
-                            >
-                                <div className="kanban__section__title">
-                                    {section.title}
-                                </div>
-                                <div className="kanban__section__content">
-                                    {
-                                        section.tasks.map((task, i) => (
-                                            <Card>
-                                                {task.title}
-                                            </Card>
-                                        ))
-                                    }
-                                </div>
-                            </div>
-                        )}
-                    </Droppable>
-                ))
-            }
-        </div>
-    </DragDropContext>
-  );
-};
+    // const onDragEnd = result => {
+    //     if (!result.destination) return
+    //     const { source, destination } = result
+    // }
 
-export default Kanban;
+    return (
+        <DragDropContext >
+            <div className="kanban">
+                {
+                    data.map((section, index) => (
+                        <Droppable
+                            key={section.id}
+                            droppableId={section.id}
+                            index={index}
+                        >
+                            {(provided) => (
+                                <div
+                                    {...provided.droppableProps}
+                                    className='kanban__section'
+                                    ref={provided.innerRef}
+                                >
+                                    <div className="kanban__section__title">
+                                        {section.title}
+                                    </div>
+                                    <div className="kanban__section__content">
+                                        {
+                                            section.tasks.map((task, index) => (
+                                                
+                                                <Draggable
+                                                    key={task.id}
+                                                    draggableId={task.id}
+                                                    index={index}
+                                                >
+                                                    {(provided, snapshot) => (
+                                                        <div
+                                                            ref={provided.innerRef}
+                                                            {...provided.draggableProps}
+                                                            {...provided.dragHandleProps}
+                                                            style={{
+                                                                ...provided.draggableProps.style,
+                                                                opacity: snapshot.isDragging ? '0.5' : '1'
+                                                            }}
+                                                        >
+                                                            <Card>
+                                                                {task.title}
+                                                            </Card>
+                                                        </div>
+                                                    )}
+                                                </Draggable>
+                                            ))
+                                        }
+                                        {provided.placeholder}
+                                    </div>
+                                </div>
+                            )}
+                        </Droppable>
+                    ))
+                }
+            </div>
+        </DragDropContext>
+    )
+}
+
+export default Kanban
